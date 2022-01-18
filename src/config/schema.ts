578d4cfb4a690ff97987da75c51b5dd2266f84e4
dw511214992 <dw511214992@gmail.com>
2022-01-18 13:26:00 +0800

    REVERT

--- src/config/schema.ts
+++ src/config/schema.ts
@@ -304,7 +304,7 @@ export const configSchema = convict<Config>({
             },
             {
                 name: 'runners',
-                id: '753c5992-7810-11ec-90d6-0242ac120003',
+                id: '25c5363b-7fe5-4683-9363-4739fab8c1bb',
                 thumbprints: ['B95A4E11225552D9C331694CAE0D833DF29B02DF', '4A79AB5164AD5D90C26A23A5479F232A210F6480'],
                 authMetadataEndpoints: [],
             },
